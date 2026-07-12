c3a05ce998f2f4765a63a784fa54c9fdc7f1d2fb
jnorris <31383711+jalnor@users.noreply.github.com>
2026-07-11 20:21:37 -0400

    Added model and admin reg.

--- mnblog/everything_important/models.py
+++ mnblog/everything_important/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
+
+#TODO  Topics for menu bar
+# Education; Religion; Jobs/Careers; Health; Technology; Government #
 
 # Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DR', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+        ARCHIVED = 'AR', 'Archived'
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    date = models.DateField()
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default='DR',
+    )
+
+    class Meta:
+        ordering = ('-publish',)
+        indexes = [models.Index(fields=['-publish'])]
+        db_table = 'blog_post'
+
+    def __str__(self):
+        return self.title
--- mnblog/everything_important/views.py
+++ mnblog/everything_important/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.templatetags.static import static
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the everything_important index!")
+    title = "Everything Important"
+    images = [
+        static('images/bv-1.webp'),
+        static('images/bv-2.webp'),
+        static('images/bv-3.webp'),
+        static('images/bv-4.webp'),
+        static('images/bv-5.webp'),
+        static('images/bv-6.webp'),
+        static('images/bv-7.webp'),
+        static('images/bv-8.webp'),
+        static('images/bv-9.webp'),
+        static('images/bv-10.webp'),
+        static('images/bv-11.webp'),
+        static('images/bv-12.webp'),
+        static('images/bv-13.webp'),
+        static('images/bv-14.webp'),
+        static('images/bv-15.webp'),
+    ]
+    return render(
+        request=request,
+        template_name='everything_important/templates/index.html',
+        context={
+            'title': title,
+            'images': images,
+        },
+    )
+
+
